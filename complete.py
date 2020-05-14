dd57638ab5b987ea378cc76bb46438858c738687
James Rosado <jamesros@boldgrid.com>
2020-05-14 16:44:17 -0400

    added walking of contents dir

--- complete.py
+++ complete.py
@@ -1,16 +1,37 @@
 # complete.py
 import sys
 
-args = sys.argv
+from os import walk
 
-name = args[0]
-content_path = args[1]
-root_path = args[2]
-category = args[3]
+class Complete:
+	def __init__( self ):
+		self.args = sys.argv
+		self.torrent_data = {
+			"name"         : self.args[1].lower().replace( " ", "."),
+			"content_path" : self.args[2].lower().replace( " ", "."),
+			"root_path"    : self.args[3].lower().replace( " ", "."),
+			"category"     : self.args[4].lower().replace( " ", ".")
+		}
+		self.torrent_data["category"] = self.getCategory()
 
-print( name )
-print( content_path )
-print( root_path )
-print( category )
+		self.parseContents()
 
-input('Press ENTER to exit')
+		self.printData()
+
+	def getCategory( self ):
+		category = 'None'
+
+		if ( 0 < len( self.torrent_data["category"] ) ):
+			category = self.torrent_data["category"]
+
+		return category
+
+	def printData( self ):
+		print( self.torrent_data )
+
+	def parseContents( self ):
+		contents_walk = {}
+		contents_walk = walk( self.torrent_data['content_path'] )
+		print( contents_walk )
+
+complete = Complete()
