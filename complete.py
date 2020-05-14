7818f85dd6eed9c0d1e4eeba7d3c2c1318d96c95
James Rosado <jamesros@boldgrid.com>
2020-05-14 17:43:27 -0400

    added movePrimaryFile

--- complete.py
+++ complete.py
@@ -1,11 +1,18 @@
 # complete.py
 import sys
+import shutil
 
-from os import listdir
+from os import listdir, walk
 
 class Complete:
 	def __init__( self ):
 		self.args = sys.argv
+
+		self.save_dirs = {
+			"tv.shows" : r"V:\TV_Shows",
+			"none"     : r"V:\Downloads"
+		}
+
 		self.torrent_data = {
 			"name"         : self.args[1].lower().replace( " ", "."),
 			"content_path" : self.args[2],
@@ -16,12 +23,10 @@ class Complete:
 
 		self.parseContents()
 
-		self.printData()
-
 	def getCategory( self ):
-		category = 'None'
+		category = 'none'
 
-		if ( 0 < len( self.torrent_data["category"] ) ):
+		if ( 0 < len( self.torrent_data["category"] ) and '.' != self.torrent_data["category"] ):
 			category = self.torrent_data["category"]
 
 		return category
@@ -31,9 +36,39 @@ class Complete:
 			print ( data )
 
 	def parseContents( self ):
-		contents_walk = {}
-		contents_walk = listdir( self.torrent_data['content_path'] )
-		print( contents_walk )
+		is_multi_pack = False
+		for ( path, directory, files ) in walk( self.torrent_data['content_path'] ):
+			if 0 < len( directory ):
+				for directory_name in directory:
+					if 'sample' in directory_name.lower():
+						continue
+					elif 'screens' in directory_name.lower():
+						continue
+					else:
+						is_multi_pack = True
+
+			if is_multi_pack:
+				break
+
+			for file_name in files:
+				if 'sample' in file_name:
+					continue
+				if file_name.endswith( '.mkv' ):
+					self.movePrimaryFile( [ path, file_name ] )
+		if is_multi_pack:
+			self.parseMultiPack()
+
+	def movePrimaryFile( self, file_info ):
+		src = file_info[0] + '/' + file_info[1]
+		if self.torrent_data['category'] in self.save_dirs.keys():
+			dest = self.save_dirs[self.torrent_data['category']]
+		else:
+			dest = self.save_dirs['none']
+
+		shutil.move( src, dest )
+
+	def parseMultiPack( self ):
+		print ( 'coming soon' )
 
 complete = Complete()
 input('Press ENTER to exit')
